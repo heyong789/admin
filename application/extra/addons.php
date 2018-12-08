3876fc4041dc7baae9da46c1190c80ef4d1bcb4f
heyon <515816332@qq.com>
2018-12-08 12:40:30 +0800

    12.8-1

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -4,8 +4,24 @@ return array (
   'autoload' => false,
   'hooks' => 
   array (
+    'ems_send' => 
+    array (
+      0 => 'faems',
+    ),
+    'ems_notice' => 
+    array (
+      0 => 'faems',
+    ),
+    'upload_after' => 
+    array (
+      0 => 'thumb',
+    ),
   ),
   'route' => 
   array (
+    '/example$' => 'example/index/index',
+    '/example/d/[:name]' => 'example/demo/index',
+    '/example/d1/[:name]' => 'example/demo/demo1',
+    '/example/d2/[:name]' => 'example/demo/demo2',
   ),
 );
